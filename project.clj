cfbbe12fa14fbf2d3e8e65c0730452c2d42ead17
Sam Johnson <sjohnson540@gmail.com>
2017-04-09 23:27:57 -0700

    Add concurrent connections

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :url "http://example.com/FIXME"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]])
--- src/moccasin/core.clj
+++ src/moccasin/core.clj
@@ -13,7 +13,7 @@
   (with-open [socket-out (io/writer socket)]
     (.write socket-out response)))
 
-;; needs to be method SP request-tartget SP http-version
+;; needs to be method SPACE request-target SPACE http-version
 (defn parse-start-line [line]
   (let [[method target version] (strings/split line #" ")]
     {:method method
@@ -34,24 +34,32 @@
 (defn valid-version?
   "Check to see if version is HTTP/1.0 or HTTP/1.1"
   [version]
-  (some #(= method %)))
+  (some #(= version %) '("HTTP/1.0" "HTTP/1.1")))
 
 ;; Parse out the message into specific parts
 ;; Starting with the "startline"
 ;; Print the incoming message
 ;; Respond to the request
 (defn handle [socket]
-  (with-open [request (io/reader socket)
-              start-line (parse-start-line (.readLine request))]
-    (println start-line)
-    (send-response "Thanks for the info!" socket)))
+  (with-open [r (io/reader socket)]
+    (let [start-line (parse-start-line (.readLine r))]
+      (println start-line)
+      (send-response "200 OK\r\n" socket))))
 
-;; Start the server!!
-(defn serve [port]
-  (with-open [server-sock (ServerSocket. port)
-              socket (.accept server-sock)]
-    (handle socket)))
+;; Start the server on [port]
+;; Loop until killed
+(defn serve [server]
+  (with-open [server-sock (ServerSocket. (:port server))]
+    (loop [socket (.accept server-sock) runs 1]
+      (future
+        (handle socket)
+        (.close socket))
+      (if (< runs 25)
+        (recur (.accept server-sock) (inc runs))))))
 
+(defrecord Moccasin [port])
+
+(def server (Moccasin. 9001))
 
 ;; Startline (request line)
 ;; first line of the HTTP request
@@ -59,5 +67,4 @@
 
 
 ;; method SP request-target SP http-version CRLF
-
 ;; MUST support GET HEAD, optional POST, PUT, DELETE, CONNECT, OPTIONS, TRACE
